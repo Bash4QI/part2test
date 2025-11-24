58d33dcf7c199ffa7c448d9898c83064205c5f06
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-24 14:01:10 -0700

    try

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import com.android.build.gradle.LibraryExtension
+import com.android.build.api.dsl.LibraryExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
@@ -29,7 +29,10 @@ allprojects {
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-fun Project.android(configuration: LibraryExtension.() -> Unit) = extensions.getByName<LibraryExtension>("android").configuration()
+//fun Project.android(configuration: LibraryExtension.() -> Unit) = extensions.getByName<LibraryExtension>("android").configuration()
+
+fun Project.android(configuration: LibraryExtension.() -> Unit) =
+    extensions.getByType(LibraryExtension::class.java).apply(configuration)
 
 subprojects {
     apply(plugin = "com.android.library")
