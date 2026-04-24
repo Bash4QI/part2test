1a08478c443e1dbc87aac2ad47f11545eb175860
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-24 14:16:43 -0600

    Test

--- InternetArchiveProvider/build.gradle.kts
+++ InternetArchiveProvider/build.gradle.kts
@@ -19,5 +19,5 @@ cloudstream {
     tvTypes = listOf("Others")
     iconUrl = "https://www.google.com/s2/favicons?domain=archive.org&sz=%size%"
 
-    isCrossPlatform = true
+    isCrossPlatform = false
 }
