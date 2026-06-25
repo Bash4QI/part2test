699e8123290ae08744d87b8f63fa8066a3f5e1f4
Bash4QI <99900010+Bash4QI@users.noreply.github.com>
2026-06-25 12:55:06 +0300

    Clean up build.gradle.kts by removing comments
    Removed comments and simplified build.gradle.kts.

--- ExAr/build.gradle.kts
+++ ExAr/build.gradle.kts
@@ -1,14 +1,8 @@
-// تعيين رقم إصدار البلجن (يجب أن يكون رقماً صحيحاً)
 version = 1
 
 cloudstream {
-    // الخصائص الأساسية ليتعرف عليها سكريبت البناء تلقائياً
     description = "My Custom Arabic Extension"
     authors = listOf("Bash4QI")
-    
-    // تحديد نوع المحتوى
     tvTypes = listOf("Movies", "TvSeries")
-    
-    // تفعيل التوافقية عبر المنصات
     isCrossPlatform = true
 }
